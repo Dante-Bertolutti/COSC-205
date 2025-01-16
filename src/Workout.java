60ceefd9391bdb1ca4241e13fa8d95452c6bff08
dauntehodgins <dauntehodgins@gmail.com>
2025-01-16 15:12:22 -0800

    workout exercise and test class

--- src/Workout.java
+++ src/Workout.java
@@ -13,14 +13,27 @@ public class Workout {
 		this.exercises=new ArrayList<>();
 	}
 	
+	public void add(Exercise e) {
+		this.exercises.add(e);
+	}
+	
 	public String toString() {
 		String result = "";
 		for(Exercise e : exercises)
-			result +=e.getName()+" ";
+			result +=e.getName()+" "+e.getWeight()+" "e.getReps()+"\n";
 		return result;
 	}
 	
-	public void add(Exercise e) {
-		exercises.add(e);
+	public String getDate() {
+		return this.date;
 	}
+	
+	public int getWorkoutId(){
+		return this.workoutId;
+	}
+	
+	public ArrayList<Exercises> getExercises(){
+		return this.exercises;
+	}
+
 }
--- src/main/Exercise.java
+++ src/main/Exercise.java
@@ -16,4 +16,41 @@ public class Exercise {
 	public String getName() {
 		return this.name;
 	}
+	public int getWeight() {
+		return this.weight;
+	}
+	public int getReps() {
+		return this.reps;
+	}
+	public int getOneRepMax(int weight, int reps){
+		if (reps==15) 
+		return w*1.5;
+	     if(reps==14) 
+		return w*1.466;
+		  if(reps==13) 
+	    return w*1.433;
+	    if(r==12) 
+		return w*1.4
+		if(r==11) 
+	    return w*1.366
+		if(r==10) 
+		return w*1.333
+	    if(r==9) 
+	    return w*1.3
+		if(r==8) 
+	    return w*1.266
+	    if(r==7) 
+		return w*1.233
+		if(r==6) 
+	    return w*1.2
+	    if(r==5) 
+	    return w*1.167
+	    if(r==4) 
+	    return w*1.133
+	    if(r==3) 
+	    return w*1.1
+	    if(r==2) 
+	    return w*1.067
+	    return w;
+	}
 }
