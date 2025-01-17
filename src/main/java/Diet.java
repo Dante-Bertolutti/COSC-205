03bebb29e875321847fd101981b586b72dbcf719
Foochini <zanetessmer@gmail.com>
2025-01-17 12:56:29 -0800

    made meal and food classes

---
+++ src/main/java/Diet.java
@@ -0,0 +1,17 @@
+public class Diet {
+    private String name;
+    private int ID;
+
+    public Diet(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+}
