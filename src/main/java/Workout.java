250e731884a30d52bde7541aa559dc92692820de
Dante-Bertolutti <dantebertolutti@outlook.com>
2025-01-17 12:19:40 -0800

    Fully Setup make new branches now

--- src/main/java/Workout.java
+++ src/main/java/Workout.java
@@ -1,39 +1,39 @@
+import java.util.ArrayList;
 
 public class Workout {
-	
-	private int workoutId;
-	
-	private String date;
-	
-	private ArrayList<Exercises> exercises;
-	
-	public Workout(int i, String d) {
-		this.workoutId= i;
-		this.date=d;
-		this.exercises=new ArrayList<>();
-	}
-	
-	public void add(Exercise e) {
-		this.exercises.add(e);
-	}
-	
-	public String toString() {
-		String result = "";
-		for(Exercise e : exercises)
-			result +=e.getName()+" "+e.getWeight()+" "e.getReps()+"\n";
-		return result;
-	}
-	
-	public String getDate() {
-		return this.date;
-	}
-	
-	public int getWorkoutId(){
-		return this.workoutId;
-	}
-	
-	public ArrayList<Exercises> getExercises(){
-		return this.exercises;
-	}
 
+    private int workoutId;
+
+    private String date;
+
+    private ArrayList<Exercise> exercises;
+
+    public Workout(int i, String d) {
+        this.workoutId = i;
+        this.date = d;
+        this.exercises = new ArrayList<>();
+    }
+
+    public void add(Exercise e) {
+        this.exercises.add(e);
+    }
+
+    public String toString() {
+        for (Exercise e : exercises) {
+            return e.getName() + " " + e.getWeight() + " " + e.getReps() + "\n";
+        }
+        return null;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public int getWorkoutId() {
+        return this.workoutId;
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return this.exercises;
+    }
 }
