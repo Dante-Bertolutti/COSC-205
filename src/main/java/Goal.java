4c6e0cbc5e0e223632865f6ea4aad95ff8b658c3
P0larKadin <kadinoake2005@gmail.com>
2025-01-17 13:30:13 -0800

    Create Goal.java

---
+++ src/main/java/Goal.java
@@ -0,0 +1,113 @@
+import java.util.Date;
+
+public class Goal {
+	private int id;
+	private int status; //0 = In Progress, 1 = success, 2 = fail
+	private String name;
+	private String desc;
+	private Date startDate;
+	private Date endDate;
+	//private User user;
+	
+//	private Goal(){
+//		id = ;
+//		status = 0;
+//		name = ;
+//		desc = ;
+//		category = ;
+//		startDate = ;
+//		endDate = ;
+//	}
+	
+	private Goal(int id, String name, String desc, Date startDate, Date endDate) {
+		this.id = id;
+		this.status = 0;
+		this.name = name;
+		this.desc = desc;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		//this.user = user;
+	}
+	
+	private Goal(int id, int status, String name, String desc, String category, Date startDate, Date endDate) {
+		this.id = id;
+		this.status = status;
+		this.name = name;
+		this.desc = desc;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		//this.user = user;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDesc() {
+		return desc;
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	public void finishGoal() {
+		this.status = 1;
+	}
+	
+	public void failGoal() {
+		this.status = 2;
+	}
+	
+	public void calculateTimeRemaining() {
+		Date currDate = new Date();
+		Date output = new Date(endDate.getTime() - currDate.getTime());
+		System.out.println();
+	}
+}
+
+
+class Main{
+	public static void main(String[] args) {
+		Date sd = new Date();
+		Date ed = new Date(sd.getTime() + 50);
+		Goal g = new Goal(1, "Test", "this is a test", sd, ed);
+		
+	}
+}
